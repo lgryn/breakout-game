96312be2ae2897ad62349fe0058af00d3f80733b
Lubomyr Grynyshyn <lgryn@softserveinc.com>
2018-05-04 18:18:04 +0300

    Add basic canvas

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
+import { Component } from 'react';
 import Layout from '../components/layout';
 
-export default () =>
-  <Layout title="Home">
-    <div className="example">Hello World!</div>
-  </Layout>
+class Index extends Component {
+  componentDidMount = () => {
+    const canvas = document.createElement('canvas');
+    canvas.id = 'myc';
+    canvas.width = 480;
+    canvas.height = 320;
+    document.getElementById('game-wrapper').appendChild(canvas);
+
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.rect(20, 40, 50, 50);
+    ctx.fillStyle = '#FF0000';
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  render() {
+    return (
+      <Layout title="Home">
+        <div id="game-wrapper" />
+      </Layout>
+    )
+  }
+}
+
+export default Index;
