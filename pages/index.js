704be92e2263a84e44c0d4397a39864344b9a199
Lubomyr Grynyshyn <lgryn@softserveinc.com>
2018-05-07 20:42:17 +0300

    Move game logic in to separate class

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
 import { Component } from 'react';
 import Layout from '../components/layout';
+import Game from '../lib/Game';
 
 class Index extends Component {
   componentDidMount = () => {
-    const canvas = document.createElement('canvas');
-    canvas.id = 'myc';
-    canvas.width = 480;
-    canvas.height = 320;
-    document.getElementById('game-wrapper').appendChild(canvas);
+    const game = new Game('breakout', 480, 320);
 
-    const ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = '#FF0000';
-    ctx.fill();
-    ctx.closePath();
+    function main() {
+      window.requestAnimationFrame(main);
+
+      game.render('game-wrapper');
+    }
+
+    main();
   }
 
   render() {
